2ea7445a8d6efd2e6be8c19b6f84cc2bcc33fa35
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-16 11:53:56 +0530

    search functionality

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function SearchBar() {
+function SearchBar({search, setSearch}) {
   return (
     <div className="flex items-center justify-center mt-2 gap-3 bg-gray-100">
       <input className='h-12 w-100 border-2 rounded-sm' 
-      type="text" placeholder="Enter the movie name"/>
+      type="text" 
+      value={search}
+      placeholder="Enter the movie name"
+      onChange={(e) => setSearch(e.target.value)} />
       <button 
       className='bg-purple-300 p-1 rounded-md cursor-pointer'
       >Search</button>
